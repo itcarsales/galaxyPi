27e70dbb0068c2c45233b3b55e0c6a7ba4e94313
Nick <itcarsales@gmail.com>
2020-03-01 11:31:59 -0600

    Add files via upload

--- iotInstaller.sh
+++ iotInstaller.sh
@@ -2,6 +2,17 @@
 #piPresents galaxyPi (updated for buster)
 #IOT Module
 
+if ! [ $(id -u) -ne 0 ]; then
+	echo "Setup cannot be run with sudo"
+	exit 1
+fi
+
+echo && read -p "Would you like to install NodeRED? (y/n)" -n 1 -r -s installRPI && echo
+if [[ $installRPI != "Y" && $installRPI != "y" ]]; then
+	echo "NodeRED install cancelled."
+	exit 1
+fi
+
 # Add keys for Mosquitto
 wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
 sudo apt-key add mosquitto-repo.gpg.key
